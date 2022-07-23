8874f0d3b2c414b9b141a7dd1b9e89bef3e2485d
Pathduck <stianlund@gmail.com>
2022-07-23 19:00:12 +0200

    Added cool loading effect

---
+++ footer.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package wp386
+ */
+?>
+
+				<footer id="colophon" class="site-footer" role="contentinfo">
+					<div class="site-info container">
+						<?php do_action( 'wp386_credits' ); ?>
+						<a href="https://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'wp386' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'wp386' ), 'WordPress' ); ?></a>
+						<span class="sep"> | </span>
+						<?php printf( __( 'Theme: %1$s by %2$s.', 'wp386' ), 'wp386', '<a href="https://github.com/keichan34" rel="designer">Keitaroh Kobayashi</a>' ); ?>
+					</div><!-- .site-info -->
+				</footer><!-- #colophon -->
+
+      </div><!-- .span9 -->
+    </div><!-- .row -->
+	</div><!-- #main -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap-386.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap-transition.js"></script>
+<script>
+    _386 = { 
+        fastLoad: false ,
+        onePass: false , 
+        speedFactor: 1.5 
+    };
+</script>
+</html>
--- header.php
+++ header.php
@@ -9,25 +9,23 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width">
-<meta name="description" content="<?php bloginfo('description'); ?>" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<meta name="description" content="<?php bloginfo('description'); ?>" />
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header" role="banner">
-
 		<div class="navbar navbar-inverse navbar-fixed-top">
 			<div class="navbar-inner">
 				<div class="container">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="brand"><?php bloginfo( 'name' ); ?></a><div id="site-description"><br/><?php $fortune_file='bofh-excuses'; $fortune_args='-s -n 90'; include '/meta/s/stian/html/inc/fortune.php'; ?></div>
-
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="brand"><?php bloginfo( 'name' ); ?></a><div id="site-description"><?php $fortune_file='bofh-excuses'; $fortune_args='-s -n 90'; include '/meta/s/stian/html/inc/fortune.php'; ?></div>
 					<?php $walker = new Bootstrap_Nav_Menu_Walker(); ?>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false, 'menu_class' => 'nav', 'walker' => $walker, 'fallback_cb' => '' ) ); ?>
 				</div>
