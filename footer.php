f04eb6417cb7dd66d55a23f9f4870f0221563633
Stian Lund <stianlund@gmail.com>
2024-08-20 22:35:25 +0200

    update

--- footer.php
+++ footer.php
@@ -7,14 +7,14 @@
  * @package wp386
  */
 ?>
-				<footer id="colophon" class="site-footer" role="contentinfo">
-					<div class="site-info container">
-						<?php do_action( 'wp386_credits' ); ?>
-						<a href="https://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'wp386' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'wp386' ), 'WordPress' ); ?></a>
-						<span class="sep"> | </span>
-						<?php printf( __( 'Theme: %1$s by %2$s.', 'wp386' ), 'wp386', '<a href="https://github.com/keichan34" rel="designer">Keitaroh Kobayashi</a>' ); ?>
-					</div><!-- .site-info -->
-				</footer><!-- #colophon -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="site-info container">
+				<?php do_action( 'wp386_credits' ); ?>
+				<a href="https://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'wp386' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'wp386' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( __( 'Theme: %1$s by %2$s.', 'wp386' ), 'wp386', '<a href="https://github.com/keichan34" rel="designer">Keitaroh Kobayashi</a>' ); ?>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
 
       </div><!-- .span9 -->
     </div><!-- .row -->
